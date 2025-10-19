19bc85e03dc3144da9d4293938d29fe47e47fb56
Luiz Garcia <luizhenrique9474@gmail.com>
2025-10-19 16:53:31 -0300

    Middleware e Autenticação de Login

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'; 
 import session from 'express-session'; 
+import verifyAuthenticate from './security/authenticate.js';
 
 const app = express();
 const PORT = 4000;
@@ -13,6 +14,31 @@ app.use(session({
         maxAge: 1000 * 60 * 15 // 15 minutes
      }
 }));
+
+app.use(express.urlencoded({ extended: true }));
+
+ app.post('/login' , (request, answer) => {
+
+    const username = request.body.usuario;
+    const password = request.body.senha;
+
+    if(username === 'admin' && password === '1234') {
+        request.session.authenticated = true;
+        answer.redirect('/private/gerenciarinteressado.html');
+    }
+    else {
+        request.session.authenticated = false;
+        answer.redirect('/public/login.html');
+    }
+});
+
+app.get("/logout", (request, answer) => {
+    request.session.authenticated = false;
+    answer.redirect('/public/login.html');
+});
+
+app.use(express.static('public'));
+app.use('/private', verifyAuthenticate, express.static('private'));
  
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
---
+++ security/authenticate.js
@@ -0,0 +1,7 @@
+export default function verifyAuthenticate(request, answer, next) {
+    if (request?.session?.authenticated) {
+        next();
+    } else {
+        answer.redirect('/login.html');
+    }
+}
